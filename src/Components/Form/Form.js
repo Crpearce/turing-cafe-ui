4d3b0b16f073595f0c777be5871378dab2f8a892
Colby Pearce <crpearce892@gmail.com>
2022-10-31 10:48:04 -0600

    add form component to app

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      date: "",
+      time: "",
+      number: "",
+    };
+  }
+  submitReservation = event => {
+    event.preventDefault();
+    const newReservation = {
+
+    }
+  }
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type="text"
+          placeholder="Date (mm/dd)"
+          name="date"
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type="text"
+          placeholder="Time"
+          name="time"
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type="number"
+          placeholder="Number of guests"
+          name="number"
+          value={this.state.number}
+          onChange={event => this.handleChange(event)}
+        />
+        <button className='submit-reservation' onClick={event => this.submitReservation(event)}> Make Reservation </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
