89c21c231f3b8a39fc22d0ef87d56e2c773c7be3
Colby Pearce <crpearce892@gmail.com>
2022-10-31 10:45:32 -0600

    initial commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Reservations from "../Components/Reservations/Reservations";
+import Form from "../Components/Form/Form";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+      error: ''
+    };
+  }
+  
+  componentDidMount() {
+    fetch("http://localhost:3001/api/v1/reservations")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error receiving Data");
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        this.setState({ reservations: data });
+      })
+      .catch((error) =>
+        this.setState({ error: "Error loading page, please try again!" })
+      );
+  }
+
+  addReservation = (newReservation) => {
+    this.setState({ reservations: [...this.state.reservations, newReservation]})
+  } 
+
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <div className="resy-form">
+          <Form addReservation={this.addReservation}/>
         </div>
-        <div className='resy-container'>
-          
+        <div className="resy-container">
+          <Reservations resyData={this.state.reservations}/>
         </div>
       </div>
-    )
+    );
   }
 }
 
