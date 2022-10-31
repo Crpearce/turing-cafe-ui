911e7f0c2d15ea521d3605d5555b1a48e58d2b54
Colby Pearce <crpearce892@gmail.com>
2022-10-31 10:46:44 -0600

    add reservations to app rendering

---
+++ src/Components/Reservations/Reservations.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "../Card/Card";
+import "./Reservations.css";
+
+function Reservations(props) {
+  console.log(props);
+  const allReservations = props.resyData.map((reservation) => {
+    return <Card 
+    id={reservation.id}
+    key={reservation.id}
+    name={reservation.name}
+    date={reservation.date}
+    time={reservation.time}
+    number={reservation.number}
+    />;
+  });
+
+  return (
+    <div className='reservations-container'>
+      <h1>{allReservations}</h1>
+    </div>
+  );
+}
+
+export default Reservations;
